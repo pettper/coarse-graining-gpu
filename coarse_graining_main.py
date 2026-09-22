13012d47ef2397c5753bb8a0b4ac40f7b47297aa
Petter <98petter@gmail.com>
2026-09-22 22:19:21 +0200

    update handling of gridpoints such we allow for small changes in size without recompilation.

--- coarse_graining_main.py
+++ coarse_graining_main.py
@@ -1,12 +1,12 @@
 # Author: Petter Persson
 # Email: petter.p.persson@gmail.com
-# Description: This file contains a listener class that computes the physical fields from particle data using the
+# Description: This file contains a class that computes the physical fields from particle data using the
 #              coarse graining method described in the document "./coarse_graining_documentation/Stress and strain in pseudo-particle solids.pdf".
 #              The implementation uses the python library JAX for GPU-acceleration.
 #
 # 2025-12-04: First public version, programmed by Petter Persson.
 import os
-from math import ceil, pi
+from math import ceil
 
 # AGX imports
 import jax
@@ -44,7 +44,8 @@ class CoarseGrainingMain:
         C_POS_KEY,
     }
     PARTICLE_PACKING_DENSITY = 0.75
-    STANDARD_BUFFER_SIZE = 128
+    STANDARD_PARTICLE_BUFFER_SIZE = 1024
+    STANDARD_GRIDPOINTS_BUFFER_SIZE = 128
 
     def __init__(
         self,
@@ -52,7 +53,6 @@ class CoarseGrainingMain:
         smoothing_length,
         particle_diameter,
         cg_batch_size=100,
-        max_num_particles=None,
         debug_prints_on=False,
     ):
         """
@@ -60,17 +60,17 @@ class CoarseGrainingMain:
             gridpoints,
             smoothing_length,
             particle_diameter,
-            max_num_particles=10000
         )
         INPUTS:
             grid: array of size np x 3, assumed to be regular grid.
             smoothing_length: the smoothing length parameter to use.
             particle_diameter: the mean particle diameter for the granular particles.
-            max_num_particles: Optional parameter, to manually put an upper limit
-                on the number of particles that fits inside the provided grid.
         """
 
-        self.gridpoints = gridpoints
+        self.gridpoints = np.zeros([10,3])  # Just set something, it is immidiately updated with correct input.
+        self.number_of_gridpoints = self.gridpoints.shape[0]
+        self.update_gridpoints(gridpoints)
+
         self.cg_batch_size = cg_batch_size
         self.debug_prints_on = debug_prints_on
 
@@ -88,8 +88,8 @@ class CoarseGrainingMain:
             print(f"NUM_CUTOFF_PARTICLES={int(os.environ.get('NUM_CUTOFF_PARTICLES', '1500'))}")
 
         # To handle adaptive buffer sizes
-        self.particle_buffer_size = self.STANDARD_BUFFER_SIZE
-        self.contact_buffer_size = self.STANDARD_BUFFER_SIZE
+        self.particle_buffer_size = self.STANDARD_PARTICLE_BUFFER_SIZE
+        self.contact_buffer_size = self.STANDARD_PARTICLE_BUFFER_SIZE
         if self.debug_prints_on:
             print(f"Particle buffer size: {self.particle_buffer_size}")
             print(f"Contact buffer size: {self.contact_buffer_size}")
@@ -124,18 +124,26 @@ class CoarseGrainingMain:
             args,
             batch_size=self.cg_batch_size,
         )
+        fields = {k: v[:self.number_of_gridpoints] for k, v in fields.items()}
         return fields
 
     def update_gridpoints(self, gridpoints):
         """
-        This function is used to update the gridpoints.
+        This function is used to update the gridpoints. 
 
         INPUTS:
-            gridpoints: New gridpoints stored in an nx3 array. The grid is assumed to be regular.
-            max_num_particles: Optional parameter, to manually put an upper limit
-                                on the number of particles that fits inside the provided grid.
+            gridpoints: New gridpoints stored in an nx3 array.
         """
-        self.gridpoints = gridpoints
+
+        assert gridpoints.ndim == 2 and gridpoints.shape[1] == 3
+        self.number_of_gridpoints = gridpoints.shape[0]
+
+        size = int(ceil(self.number_of_gridpoints / self.STANDARD_GRIDPOINTS_BUFFER_SIZE)*self.STANDARD_GRIDPOINTS_BUFFER_SIZE)
+        if not size == self.gridpoints.shape[0]:
+            # To pad with something likely outside particle domain
+            self.gridpoints = self._buffer_pad(np.asarray(gridpoints), size, pad_value=9999999.0)
+        else:
+            self.gridpoints[:self.number_of_gridpoints] = np.asarray(gridpoints) 
 
     def set_particle_diameter(self, particle_diameter):
         self.params["particleDiameter"] = particle_diameter
@@ -155,22 +163,16 @@ class CoarseGrainingMain:
             """
             return np.flatnonzero(np.all((pos >= mins) & (pos <= maxs), axis=1))
 
-        def gather_zero_padded(buffer, indices, size):
-            dtype = jax.dtypes.canonicalize_dtype(buffer.dtype)
-            out = np.zeros((size, *buffer.shape[1:]), dtype=dtype)
-            out[: indices.size] = buffer[indices]
-            return jnp.asarray(out)
-
         smoothing_length = self.params["smoothingLength"]
-        mins = self.gridpoints.min(axis=0) - 3.0 * smoothing_length
-        maxs = self.gridpoints.max(axis=0) + 3.0 * smoothing_length
+        mins = self.gridpoints[:self.number_of_gridpoints].min(axis=0) - 3.0 * smoothing_length
+        maxs = self.gridpoints[:self.number_of_gridpoints].max(axis=0) + 3.0 * smoothing_length
         particleIndices = pick_indices(input_buffers[P_POS_KEY], mins, maxs)
         contactIndices = pick_indices(input_buffers[C_POS_KEY], mins, maxs)
-        n_particles, n_contacts = self._set_buffer_sizes(particleIndices.size, contactIndices.size)
+        n_particles, n_contacts = self._set_particle_buffer_sizes(particleIndices.size, contactIndices.size)
 
         for key, buffer in input_buffers.items():
             if key in [P_POS_KEY, P_VEL_KEY, P_DISP_KEY, P_MASS_KEY]:
-                input_buffers[key] = gather_zero_padded(buffer, particleIndices, n_particles)
+                input_buffers[key] = jnp.asarray(self._buffer_pad(buffer[particleIndices], n_particles, pad_value=0.0))
             elif key in [
                 C_FORCE_KEY,
                 C_POS_KEY,
@@ -178,7 +180,7 @@ class CoarseGrainingMain:
                 C_TANGENT_U_KEY,
                 C_TANGENT_V_KEY,
             ]:
-                input_buffers[key] = gather_zero_padded(buffer, contactIndices, n_contacts)
+                input_buffers[key] = jnp.asarray(self._buffer_pad(buffer[contactIndices], n_contacts, pad_value=0.0))
         return input_buffers
 
     def _validate_input_buffers(self, input_buffers):
@@ -188,13 +190,13 @@ class CoarseGrainingMain:
             raise KeyError(f"Missing required keys: {missing_keys}")
         return input_buffers
 
-    def _set_buffer_sizes(self, num_particles, num_contacts):
+    def _set_particle_buffer_sizes(self, num_particles, num_contacts):
         """
         Responsible for setting the number of particles and contacts that are stored in the buffer. Strictly increasing to avoid recompilation / cache problems.
         """
 
         def update_size(count, current_size, print_label=""):
-            new_size = ceil(count / self.STANDARD_BUFFER_SIZE) * self.STANDARD_BUFFER_SIZE
+            new_size = int(ceil(count / self.STANDARD_PARTICLE_BUFFER_SIZE) * self.STANDARD_PARTICLE_BUFFER_SIZE)
             if new_size > current_size:
                 size = new_size
                 if self.debug_prints_on:
@@ -208,3 +210,16 @@ class CoarseGrainingMain:
         self.contact_buffer_size = update_size(num_contacts, self.contact_buffer_size, "contact")
 
         return self.particle_buffer_size, self.contact_buffer_size
+
+    @staticmethod
+    def _buffer_pad(buffer, size, pad_value=0.0):
+        """
+        Adds a zero padding along axis=0, result is (size, -1). Using pad_value=None does not assign any value to the padded entries, useful for performance.
+        """
+        n = buffer.shape[0]
+        dtype = jax.dtypes.canonicalize_dtype(buffer.dtype)
+        out = np.empty((size, *buffer.shape[1:]), dtype=dtype)
+        out[:n] = buffer
+        if pad_value is not None:
+            out[n:] = pad_value
+        return out
